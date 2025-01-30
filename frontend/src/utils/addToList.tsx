f5b9b7c123f3bceff91ead59f268ab2794cc6a2b
Portavion <156545430+Portavion@users.noreply.github.com>
2025-01-30 00:36:46 +0000

    fix: type error

--- frontend/src/utils/addToList.tsx
+++ frontend/src/utils/addToList.tsx
@@ -2,24 +2,35 @@ async function addToList(
   token: string,
   listId: number,
   bikePointId: string,
-): Promise<BikePoint[]> {
-  const body = { token: token, listId: listId, bikePointToAdd: bikePointId };
+): Promise<string[]> {
+  // const body = { token: token, listId: listId, bikePointToAdd: bikePointId };
 
-  const formBody = Object.keys(body)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(body[key]))
-    .join("&");
+  const formBody =
+    encodeURIComponent("listId") +
+    "=" +
+    encodeURIComponent(listId) +
+    "&" +
+    encodeURIComponent("bikePoint") +
+    "=" +
+    encodeURIComponent(bikePointId);
+  // const formBody = Object.keys(body)
+  //   .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(body[key]))
+  //   .join("&");
 
   try {
-    await fetch(`${process.env.VITE_BASE_URL}/api/v1/bikepointslists/`, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        Authorization: `Bearer ${token}`,
+    const bikePointResponse = await fetch(
+      `${process.env.VITE_BASE_URL}/api/v1/bikepointslists/`,
+      {
+        method: "PUT",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${token}`,
+        },
+        body: formBody,
       },
-      body: formBody,
-    });
-    const bikePointData: BikePoint[] = await bikePointResponse.json();
-    bikePoints = bikePoints.concat(bikePointData);
+    );
+    const updatedList: BikePointList = await bikePointResponse.json();
+    const bikePoints = updatedList.bikePointsIds;
     return bikePoints;
   } catch (error) {
     console.log(error);
