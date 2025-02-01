014669f760f6e34249743686ff3fabc9984c2b4c
Portavion <156545430+Portavion@users.noreply.github.com>
2025-02-01 15:17:58 +0000

    feat: styling signup page

--- frontend/src/features/auth/SignupForm/SignupForm.tsx
+++ frontend/src/features/auth/SignupForm/SignupForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./SignupForm.module.css";
 
 function SignupForm() {
   const [username, setUsername] = useState("");
@@ -33,59 +32,73 @@ function SignupForm() {
         }
       });
   }
-
   return (
-    <form onSubmit={submitNewUser}>
-      <div className={styles.formSection}>
-        <label htmlFor="username">Username:</label>
-        <br />
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+    <div className="flex flex-col gap-6">
+      <div className="overflow-hidden">
+        <div className="">
+          <form onSubmit={submitNewUser} className="p-6 md:p-8">
+            <div className="flex flex-col gap-6 items-center">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Signup</h1>
+              </div>
+              <div className="grid gap-2">
+                <label htmlFor="username">Username:</label>
+                <input
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="John"
+                  className="flex h-9 w-60 rounded-md border border-input bg-teal-900 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <label htmlFor="email">Email:</label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@gmail.com"
+                  className="flex h-9 w-60 rounded-md border border-input bg-teal-900 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                  value={email}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <label htmlFor="password">Password:</label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="flex h-9 w-60 rounded-md border border-input bg-teal-900 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <label htmlFor="confirm-password">Confirm Password:</label>
+                <input
+                  id="confirm-password"
+                  type="password"
+                  name="confirm-password"
+                  className="flex h-9 w-60 rounded-md border border-input bg-teal-900 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                  value={passwordConfirmation}
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  required
+                />
+              </div>
+              <button type="submit" className="w-20 h-9 bg-teal-700">
+                Signup
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className={styles.formSection}>
-        <label htmlFor="email">Email:</label>
-        <br />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          name="email"
-          id="email"
-          required
-        />
-      </div>
-      <div className={styles.formSection}>
-        <label htmlFor="password">Password:</label>
-        <br />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className={styles.formSection}>
-        <label htmlFor="confirm-password">Confirm Password:</label>
-        <br />
-        <input
-          type="password"
-          name="confirm-password"
-          id="confirm-password"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    </div>
   );
 }
 
---
+++ frontend/src/features/auth/SignupForm/SignupForm_ss.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./SignupForm.module.css";
+
+function SignupForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const navigate = useNavigate();
+
+  function submitNewUser(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+
+    fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, email, password, passwordConfirmation }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "success") {
+          setUsername("");
+          setEmail("");
+          setPassword("");
+          setPasswordConfirmation("");
+          navigate("/");
+        } else {
+          alert(data.message);
+        }
+      });
+  }
+
+  return (
+    <form onSubmit={submitNewUser}>
+      <div className={styles.formSection}>
+        <label htmlFor="username">Username:</label>
+        <br />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div className={styles.formSection}>
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          id="email"
+          required
+        />
+      </div>
+      <div className={styles.formSection}>
+        <label htmlFor="password">Password:</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className={styles.formSection}>
+        <label htmlFor="confirm-password">Confirm Password:</label>
+        <br />
+        <input
+          type="password"
+          name="confirm-password"
+          id="confirm-password"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export { SignupForm };
