f48dd434515763cde599dcbb7fa2a16722eb4d22
Portavion <156545430+Portavion@users.noreply.github.com>
2025-02-05 12:15:37 +0000

    feat: update every 5 minutes to save bandwidth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -69,7 +69,7 @@ app.use((_req, res, next) => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-setInterval(updateBikePointsTable, 1000 * 120); // updates every 2min: 1000ms * 120s
+setInterval(updateBikePointsTable, 1000 * 60 * 5); // updates every 2min: 1000ms * 60s * 5
 
 app.use("/api/v1/", apiV1Router);
 app.use(errorHandler);
