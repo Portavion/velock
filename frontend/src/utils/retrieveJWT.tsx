8b7c6d19c666669e8e1c2c0bd943c1e8b4044bae
Portavion <156545430+Portavion@users.noreply.github.com>
2025-02-14 11:37:15 +0000

    fix: check token expiry and delete if expired to force login

--- frontend/src/utils/retrieveJWT.tsx
+++ frontend/src/utils/retrieveJWT.tsx
@@ -4,7 +4,29 @@ function useRetrieveJWT() {
   const [jwtToken, setJwtToken] = useState<string>("");
 
   useEffect(() => {
+    const checkTokenExpiry = async () => {
+      if (jwtToken !== "") {
+        const isTokenValidResponse = await fetch(
+          `${import.meta.env.VITE_BASE_URL}/api/v1/auth/isTokenValid`,
+          {
+            method: "GET",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          },
+        );
+        if (isTokenValidResponse.status === 200) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    };
     setJwtToken(localStorage.getItem("jwt-token") || "");
+    if (!checkTokenExpiry()) {
+      localStorage.removeItem("jwt-token");
+    }
   }, [jwtToken]);
   return jwtToken;
 }
