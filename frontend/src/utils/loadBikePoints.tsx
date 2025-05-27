0f8895b2373ed58cf75ec48a2c58c7b02d49a241
Portavion <portalier.g@gmail.com>
2025-05-27 14:53:15 +0100

    feat: Promise.all for parallel fetching bikePoints

--- frontend/src/utils/loadBikePoints.tsx
+++ frontend/src/utils/loadBikePoints.tsx
@@ -1,20 +1,48 @@
 async function loadBikePoints(token: string, activeList: BikePointList) {
-  const bikePoints: BikePoint[] = [];
+  // const bikePoints: BikePoint[] = [];
   try {
+    const urlsToFetch: string[] = [];
     for (const bikePointId of activeList.bikePointsIds) {
-      const bikePointResponse = await fetch(
+      urlsToFetch.push(
         `${import.meta.env.VITE_BASE_URL}/api/v1/bikepoints/id/?id=${bikePointId}`,
-        {
-          method: "GET",
-          headers: {
-            "content-type": "application/x-www-form-urlencoded",
-            Authorization: `Bearer ${token}`,
-          },
-        },
       );
-      const bikePoint: BikePoint = await bikePointResponse.json();
-      bikePoints.push(bikePoint);
+      //   const bikePointResponse = await fetch(
+      //     `${import.meta.env.VITE_BASE_URL}/api/v1/bikepoints/id/?id=${bikePointId}`,
+      //   {
+      //     method: "GET",
+      //     headers: {
+      //       "content-type": "application/x-www-form-urlencoded",
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //     },
+      //   );
     }
+
+    const fetchURLs = async (
+      urls: string[],
+    ): Promise<BikePoint[] | undefined> => {
+      try {
+        const promises = urls.map((url) =>
+          fetch(url, {
+            method: "GET",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+        );
+        const responses = await Promise.all(promises);
+        const data = await Promise.all(
+          responses.map((response) => response.json()),
+        );
+        return data;
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    };
+
+    const bikePoints = fetchURLs(urlsToFetch);
+
     return bikePoints;
   } catch (error) {
     console.log(error);
